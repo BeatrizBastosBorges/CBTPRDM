39e54a4a7dc08f7e16fd854654b965ce16bba7ba
Beatriz Bastos <biaborges.estudante@gmail.com>
2025-09-06 17:16:12 -0300

    Initial Commit

---
+++ CBTPRDM.TP01/CBTPRDM.TP01/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CBTPRDM.TP01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CBTPRDM.TP01/CBTPRDM.TP01/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace CBTPRDM.TP01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOkClicked(object sender, EventArgs e)
+        {
+            string id = IdEntry.Text;
+            string senha = PassEntry.Text;
+
+            if (id == "admin" && senha == "senha@dmin")
+            {
+                DisplayAlert("Sucesso", "Logou com sucesso", "OK");
+            }
+            else
+            {
+                DisplayAlert("Erro", "Login não autorizado.", "Tentar Novamente");
+            }
+        }
+
+        private void OnLimparClicked(object sender, EventArgs e)
+        {
+            IdEntry.Text = string.Empty;
+            PassEntry.Text = string.Empty;
+
+            IdEntry.Focus();
+        }
+
+        private void OnCreditosClicked(object sender, EventArgs e)
+        {
+            string autores = "Desenvolvido por:\n- Beatriz Bastos Borges\n- Miguel Luizatto Alves";
+            DisplayAlert("Créditos", autores, "Fechar");
+        }
+    }
+
+}
